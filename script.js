81e9d6a6380fbae288be30bcc529296f8c60a39c
saulwade <saulwade29@gmail.com>
2023-03-17 20:41:00 -0600

    added the rest of the javascript code

--- script.js
+++ script.js
@@ -26,4 +26,81 @@ const questions = [
       answer: 1,
     },
   ];
+
+let currentQuestion = 0;
+let timeLeft = 60;
+let timerId;
+let quizScore = 0;
+
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    startBtn.classList.add("hidden");
+    quizContainer.classList.remove("hidden");
+    timerId = setInterval(updateTimer, 1000);
+    displayQuestion();
+  }
   
+  function updateTimer() {
+    timeLeft--;
+    timeRemaining.textContent = timeLeft;
+  
+    if (timeLeft <= 0) {
+      clearInterval(timerId);
+      endQuiz();
+    }
+  }
+  
+  function displayQuestion() {
+    const current = questions[currentQuestion];
+    question.textContent = current.question;
+    options.innerHTML = "";
+  
+    current.options.forEach((option, index) => {
+      const button = document.createElement("button");
+      button.textContent = option;
+      button.addEventListener("click", () => {
+        checkAnswer(index);
+      });
+      options.appendChild(button);
+    });
+  }
+  
+  function checkAnswer(selected) {
+    const correct = questions[currentQuestion].answer;
+  
+    if (selected === correct) {
+      quizScore++;
+    } else {
+      timeLeft -= 10;
+    }
+  
+    currentQuestion++;
+  
+    if (currentQuestion < questions.length) {
+      displayQuestion();
+    } else {
+      clearInterval(timerId);
+      endQuiz();
+    }
+  }
+  
+  function endQuiz() {
+    quizContainer.classList.add("hidden");
+    resultContainer.classList.remove("hidden");
+    scoreElement.textContent = quizScore;
+    saveBtn.addEventListener("click", saveResult);
+  }
+  
+  function saveResult() {
+    const userInitials = initials.value;
+    if (userInitials !== "") {
+      const savedScores = JSON.parse(localStorage.getItem("scores")) || [];
+      const newScore = { initials: userInitials, score: quizScore };
+      savedScores.push(newScore);
+      localStorage.setItem("scores", JSON.stringify(savedScores));
+      alert("Your score has been saved.");
+    } else {
+      alert("Please enter your initials.");
+    }
+  }
